39befe5f129492a4f4967ff41b86de65caaa5483
ccls14 <1204246010@qq.com>
2017-04-06 22:55:37 +0800

    new file:   .idea/compiler.xml 	new file:   .idea/copyright/profiles_settings.xml 	new file:   .idea/misc.xml 	new file:   .idea/modules.xml 	new file:   .idea/selenium_test.iml 	new file:   .idea/vcs.xml 	new file:   .idea/workspace.xml 	new file:   je/je.iml 	new file:   je/pom.xml 	new file:   je/src/main/java/HelloWord.java 	new file:   je/target/classes/HelloWord.class

---
+++ je/src/main/java/HelloWord.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Administrator on 2017/4/6.
+ */
+public class HelloWord {
+    public static void main(String args[]) {
+        // 向控制台输出信息
+        System.out.println("欢迎java01班的同学");
+    }
+}
