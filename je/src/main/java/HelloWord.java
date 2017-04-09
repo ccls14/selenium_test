92588e3bc99b062e5fde4317918162bc9be65631
ccls14 <1204246010@qq.com>
2017-04-09 22:56:13 +0800

    new file:   .idea/libraries/Maven__cglib_cglib_nodep_3_2_4.xml 	new file:   .idea/libraries/Maven__com_beust_jcommander_1_64.xml 	new file:   .idea/libraries/Maven__com_google_code_gson_gson_2_8_0.xml 	new file:   .idea/libraries/Maven__com_google_guava_guava_21_0.xml 	new file:   .idea/libraries/Maven__commons_codec_commons_codec_1_10.xml 	new file:   .idea/libraries/Maven__commons_collections_commons_collections_3_2_2.xml 	new file:   .idea/libraries/Maven__commons_io_commons_io_2_4.xml 	new file:   .idea/libraries/Maven__commons_logging_commons_logging_1_2.xml 	new file:   .idea/libraries/Maven__junit_junit_4_12.xml 	new file:   .idea/libraries/Maven__log4j_log4j_1_2_9.xml 	new file:   .idea/libraries/Maven__net_java_dev_jna_jna_4_1_0.xml 	new file:   .idea/libraries/Maven__net_java_dev_jna_jna_platform_4_1_0.xml

--- je/src/main/java/HelloWord.java
+++ je/src/main/java/HelloWord.java
@@ -1,9 +1,12 @@
+import org.testng.annotations.Test;
+
 /**
  * Created by Administrator on 2017/4/6.
  */
 public class HelloWord {
-    public static void main(String args[]) {
+    @Test
+    public void test() {
         // 向控制台输出信息
-        System.out.println("欢迎java01班的同学");
+        System.out.println("hello java01");
     }
 }
