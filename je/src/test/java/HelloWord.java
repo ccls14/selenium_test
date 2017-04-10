546f2aba5a5211b9da4fc34ea3ef1a1e7b09efee
ccls14 <1204246010@qq.com>
2017-04-10 22:07:12 +0800

    modified:   .idea/misc.xml 	modified:   .idea/workspace.xml 	new file:   je/src/test/java/HelloWord.class 	renamed:    je/src/main/java/HelloWord.java -> je/src/test/java/HelloWord.java 	new file:   je/src/test/java/test-output/DataRiver/dq.html 	new file:   je/src/test/java/test-output/DataRiver/dq.xml 	new file:   je/src/test/java/test-output/bullet_point.png 	new file:   je/src/test/java/test-output/collapseall.gif 	new file:   je/src/test/java/test-output/emailable-report.html 	new file:   je/src/test/java/test-output/failed.png 	new file:   je/src/test/java/test-output/index.html 	new file:   je/src/test/java/test-output/jquery-1.7.1.min.js 	new file:   je/src/test/java/test-output/junitreports/TEST-HelloWord.xml 	new file:   je/src/test/java/test-output/navigator-bullet.png

--- je/src/test/java/HelloWord.java
+++ je/src/test/java/HelloWord.java
@@ -5,8 +5,9 @@ import org.testng.annotations.Test;
  */
 public class HelloWord {
     @Test
-    public void test() {
+    public  void test() {
+    //public static void main(String args[]) {
         // 向控制台输出信息
-        System.out.println("hello java01");
+        System.out.println("hello java01 sfdss");
     }
 }
